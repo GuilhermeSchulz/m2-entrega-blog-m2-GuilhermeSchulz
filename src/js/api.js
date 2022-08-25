a35f5563cd3657182fad504b8792fd9efe31eec9
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-08-25 14:26:31 -0300

    Link entre arquivos

---
+++ src/js/api.js
@@ -0,0 +1,39 @@
+export class Api {
+    static baseUrl = "https://blog-m2.herokuapp.com";
+
+    static headers = {
+        "Content-Type": "application/json",
+    };
+
+    static async login(data){
+
+    }
+
+    static async cadastrar(data){
+
+    }
+
+    static async buscarUser(id){
+
+    }
+
+    static async buscarPostagem(){
+
+    }
+
+    static async buscarPostagemId(id){
+
+    }
+
+    static async criarPost(data){
+
+    }
+
+    static async modificarPost(id){
+
+    }
+
+    static async deletarPost(id){
+
+    }
+}
