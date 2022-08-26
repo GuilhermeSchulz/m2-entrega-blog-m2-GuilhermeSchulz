73a4f0f65e14e47601ffd3ba3674e8813f924ec0
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-08-26 17:33:26 -0300

    login e cadastro api request

--- src/js/api.js
+++ src/js/api.js
@@ -5,35 +5,64 @@ export class Api {
         "Content-Type": "application/json",
     };
 
-    static async login(data){
-
-    }
+    static token = "";
 
     static async cadastrar(data){
+        await fetch(`${this.baseUrl}/users/register`,{
+            method: "POST",
+            headers:  this.headers,
+            body: JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err))
+        
+    };
 
-    }
+    static async login(data){
+        let response =  await fetch(`${this.baseUrl}/users/login`,{
+            method: "POST",
+            headers:  this.headers,
+            body: JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            this.token = res.token;
+            localStorage.setItem("token@blogKenzie", this.token);
+            localStorage.setItem("userId@blogKenzie", res.userId);
+            return res;
+        })
+        .catch((error) => error);
+        
+        if (this.token){
+            window.location.href = "./src/pages/home.html"
+        }else{
+            return response;
+        }
+
+    };    
 
     static async buscarUser(id){
 
-    }
+    };
 
     static async buscarPostagem(){
 
-    }
+    };
 
     static async buscarPostagemId(id){
 
-    }
+    };
 
     static async criarPost(data){
 
-    }
+    };
 
     static async modificarPost(id){
 
-    }
+    };
 
     static async deletarPost(id){
 
-    }
+    };
 }
--- src/js/scriptIndex.js
+++ src/js/scriptIndex.js
@@ -1,1 +1,54 @@
 import {Api} from './api.js';
+
+class User{
+
+    static cadastra(){
+        let nomeCadastro    = document.getElementsByName("nomeCadastro")[0].value;
+        let emailCadastro   = document.getElementsByName("emailCadastro")[0].value;
+        let imagemCadastro  = document.getElementsByName("imagemCadastro")[0].value;
+        let senhaCadastro   = document.getElementsByName("senhaCadastro")[0].value;
+
+        let data = {
+            username: nomeCadastro,
+            email: emailCadastro,
+            avatarUrl: imagemCadastro,
+            password: senhaCadastro
+        }
+
+        Api.cadastrar(data);
+    }
+
+    static async login(){
+        let emailLogin      = document.getElementsByName("emailLogin")[0].value;
+        let senhaLogin      = document.getElementsByName("senhaLogin")[0].value;
+
+        let data = {
+            email: emailLogin,
+            password: senhaLogin
+        }
+
+        console.log(await Api.login(data));
+    }
+}
+
+class Button{
+
+    static cadastrar(){
+        let buttonCadastrar = document.getElementById("buttonCadastrar");
+        buttonCadastrar.addEventListener("click",function(event){
+            event.preventDefault();
+            User.cadastra();
+        })
+    }
+
+    static login(){
+        let buttonLogin     = document.getElementById("buttonLogin");
+        buttonLogin.addEventListener("click", async function(event){
+            event.preventDefault();
+            User.login();
+        })
+    }
+}
+
+Button.cadastrar();
+Button.login();
